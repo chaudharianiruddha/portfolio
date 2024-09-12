79492c6774e0e64c80377919d8a565473765ad6e
chaudharianiruddha <chaudharianiruddha19@gmail.com>
2024-09-12 15:32:28 -0400

    Update skill.bar.js

--- assets/js/skill.bar.js
+++ assets/js/skill.bar.js
@@ -2,21 +2,17 @@
     "use strict";
 
     /*==========================
-        Skills Area Animation
+        About Area
     ============================*/
+
+    var offsetTop = $('#skills').offset().top;
     $(window).on('scroll', function () {
-        var offsetTop = $('#skills').offset().top; // Ensure the offset is inside the scroll event handler
-        var windowHeight = $(window).height();
-        var scrollTop = $(window).scrollTop();
-        
-        if (scrollTop + windowHeight > offsetTop) {
-            $('.skillbar').each(function () {
-                var skillBar = $(this).find('.skillbar-bar');
-                if (!skillBar.hasClass('animated')) { // To prevent re-triggering animation
-                    skillBar.animate({
-                        width: $(this).attr('data-percent')
-                    }, 1000).addClass('animated'); // Adding a class to prevent duplicate animations
-                }
+        var height = $(window).height();
+        if ($(window).scrollTop() + height > offsetTop) {
+            jQuery('.skillbar').each(function () {
+                jQuery(this).find('.skillbar-bar').animate({
+                    width: jQuery(this).attr('data-percent')
+                }, 1000);
             });
         }
     });
