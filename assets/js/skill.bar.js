ba64179e3941bcf582489ad40825328a3a8ab6c0
chaudharianiruddha <chaudharianiruddha19@gmail.com>
2024-09-12 15:22:04 -0400

    Update skill.bar.js

--- assets/js/skill.bar.js
+++ assets/js/skill.bar.js
@@ -5,22 +5,6 @@
         About Area
     ============================*/
 
-
-    <script>
-    // Trigger animation when the skill bars are in view
-    window.addEventListener('scroll', function () {
-        var skillBars = document.querySelectorAll('.skill-bar-fill');
-        skillBars.forEach(function (bar) {
-            var barPosition = bar.getBoundingClientRect().top;
-            var screenPosition = window.innerHeight / 1.3;
-
-            if (barPosition < screenPosition) {
-                bar.style.width = bar.getAttribute('data-percentage');
-            }
-        });
-    });
-</script>
-
     var offsetTop = $('#skills').offset().top;
     $(window).on('scroll', function () {
         var height = $(window).height();
